9c9813910ab878c681a038598720442e8f41d275
Cam <112639963+CameronMeile@users.noreply.github.com>
2023-12-22 13:03:33 +0100

    chore: Added Swagger Tags fix: Author added to POST

--- routes/tasks.js
+++ routes/tasks.js
@@ -87,29 +87,51 @@ router.use(verifyAuthentication);
 // GET /tasks Endpunkt, welcher eine Liste aller Tasks zurück gibt
 router.get('/', (req, res) => {
     res.status(200).json(tasks);
+    /*
+    #swagger.path = '/tasks'
+    #swagger.produces = ['application/json']
+    #swagger.tags = ['tasks']
+    #swagger.summary = 'Eine Liste aller Tasks wird zurück gegeben.'
+    #swagger.responses[200] = {
+        schema: { $ref: '#/definitions/tasks' }
+    }  
+    */
 });
 
 // POST /tasks Endpunkt, der einen neuen Task erstellt und diesen zurückgibt
 router.post('/', (req, res) => {
-
     const newTask = req.body;
     newTask.id = randomUUID();
+    newTask.author = req.session.token;
     newTask.erstellungsdatum = new Date().toISOString();
     newTask.erfüllungsdatum = null;
 
     tasks.push(newTask);
     res.status(201).send(newTask);
+    /*
+    #swagger.path = '/tasks'
+    #swagger.produces = ['application/json']
+    #swagger.consumes = ['application/json']
+    #swagger.tags = ['tasks']
+    #swagger.summary = 'Mit einem JSON POST Request, eine neue Task hinzufügen. '
+    #swagger.responses[201] = {
+        schema: { $ref: '#/definitions/tasks' }
+    }  
+
+        #swagger.parameters['body'] = {
+            in: 'body',
+            description: 'User data.',
+            required: true,
+            schema: {
+                title: "New Title"
+            }
+        }
+    */
 });
 
-// Example request body:
-// {
-// "title": "New Task /POST"
-// }
-
 // GET /tasks/{id} Endpunkt, welcher einen einzelnen Task zurück gibt
-router.get('/:id', (req, res) => {
+router.get('/:id', (req, res) => {    
     const taskId = req.params.id;
-
     const task = tasks.find(task => task.id === taskId);
 
     if (!task) {
@@ -117,6 +139,36 @@ router.get('/:id', (req, res) => {
     }
 
     return res.status(200).json(task);
+     /*
+    #swagger.path = '/tasks/{id}'
+    #swagger.produces = ['application/json']
+    #swagger.consumes = ['application/json']
+    #swagger.tags = ['tasks']
+    #swagger.summary = 'Eine bestehende Task via {id} ausgeben.'
+    #swagger.responses[200] = {
+        schema: { $ref: '#/definitions/tasks' }
+    }  
+    #swagger.responses[404] = {
+        schema: { $ref: '#/definitions/tasks' }
+    }  
+
+        #swagger.parameters['id'] = {
+            in: 'path',
+            description: 'Task ID.',
+            required: true,
+            type: 'string'
+        }
+
+        #swagger.parameters['body'] = {
+            in: 'body',
+            description: 'User data.',
+            required: true,
+            schema: {
+                title: "New Title",
+                password: "1234"
+            }
+        }
+    */
 });
 
 // PATCH /task/{id} Endpunkt, welcher den bestehenden Task verändert und diesen zurück gibt
@@ -134,14 +186,38 @@ router.patch('/:id', (req, res) => {
     task.erfüllungsdatum = erfüllungsdatum;
 
     return res.status(200).json(task);
-});
+      /*
+    #swagger.path = '/tasks/{id}'
+    #swagger.produces = ['application/json']
+    #swagger.consumes = ['application/json']
+    #swagger.tags = ['tasks']
+    #swagger.summary = 'Mit einem JSON PATCH Request, ein bestehende Task via {id} bearbeiten.'
+    #swagger.responses[200] = {
+        schema: { $ref: '#/definitions/tasks' }
+    }  
+    #swagger.responses[404] = {
+        schema: { $ref: '#/definitions/tasks' }
+    }  
 
-// Example request body:
-// {
-// "title": "Changed Title",
-// "erstellungsdatum": "2023-12-22T10:30:00.000Z",
-// "erfüllungsdatum": "2023-12-31T23:59:59.000Z"
-// }
+        #swagger.parameters['id'] = {
+            in: 'path',
+            description: 'Task ID.',
+            required: true,
+            type: 'string'
+        }
+
+        #swagger.parameters['body'] = {
+            in: 'body',
+            description: 'User data.',
+            required: true,
+            schema: {
+                "title": "Changed Title",
+                "erstellungsdatum": "2023-12-22T10:30:00.000Z",
+                "erfüllungsdatum": "2023-12-31T23:59:59.000Z"
+            }
+        }
+    */
+});
 
 // DELETE /task/{id} Endpunkt, welcher den bestehenden Task löscht
 router.delete('/:id', (req, res) => {
@@ -154,6 +230,26 @@ router.delete('/:id', (req, res) => {
 
     const deletedTask = tasks.splice(taskIndex, 1)[0];
     return res.status(200).json(deletedTask);
+      /*
+    #swagger.path = '/tasks/{id}'
+    #swagger.produces = ['application/json']
+    #swagger.consumes = ['application/json']
+    #swagger.tags = ['tasks']
+    #swagger.summary = 'Mit der {id} eine bestehende Task löschen.'
+    #swagger.responses[200] = {
+        schema: { $ref: '#/definitions/tasks' }
+    }  
+    #swagger.responses[404] = {
+        schema: { $ref: '#/definitions/tasks' }
+    }  
+
+        #swagger.parameters['id'] = {
+            in: 'path',
+            description: 'Task ID.',
+            required: true,
+            type: 'string'
+        }
+    */
 });
 
 module.exports = router;
