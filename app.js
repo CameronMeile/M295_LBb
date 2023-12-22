59bb439c0593686dc1e5151a15befcd43d26c4de
Cam <112639963+CameronMeile@users.noreply.github.com>
2023-12-22 09:48:28 +0100

    chore: /login updated / new logic

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const session = require('express-session');
 // const swaggerUi = require('swagger-ui-express');
 // const swaggerDocument = require('./swagger.json');
 const app = express();
@@ -48,6 +49,35 @@ const { randomUUID } = require('node:crypto');
 
 const tasksRouter = require('./routes/tasks');
 app.use('/tasks', tasksRouter);
+
+app.use(session({
+    secret: 'supersecret',
+    resave: false,
+    saveUninitialized: true
+}));
+
+// POST /login Endpunkt, welcher die Credentials entgegennimmt, überprüft und ein Token oder Cookie zurück gibt
+app.post('/login', (req, res) => {
+    const { email, password } = req.body;
+
+    // Check if the credentials are valid
+    if (email && password && password === 'm295') {
+        // Store the user's authentication status in the session
+        req.session.isAuthenticated = true;
+
+        // Generate a token (if needed) and store it in the session or any other required data
+        req.session.token = randomUUID();
+
+        // Return a JSON response with the token or any other relevant data
+        res.status(200).json({
+            message: 'Login successful',
+            token: req.session.token
+        });
+    } else {
+        // Invalid credentials
+        res.status(401).json({ error: 'Invalid credentials' });
+    }
+});
 
 // Server Listens on PORT:3000
 app.listen(port, () => {
